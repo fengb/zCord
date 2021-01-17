a4abb5731742936e75b2bd990471cb9fecff3d98
Eleanor Bartle <eleanor@eleanor-nb.com>
2021-01-18 05:20:57 +1100

    Number Zen
    :^)

--- src/main.zig
+++ src/main.zig
@@ -195,19 +195,19 @@ const Context = struct {
                     .title = "For Great Justice",
                     .description = &.{
                         \\```
-                        \\* Communicate intent precisely.
-                        \\* Edge cases matter.
-                        \\* Favor reading code over writing code.
-                        \\* Only one obvious way to do things.
-                        \\* Runtime crashes are better than bugs.
-                        \\* Compile errors are better than runtime crashes.
-                        \\* Incremental improvements.
-                        \\* Avoid local maximums.
-                        \\* Reduce the amount one must remember.
-                        \\* Focus on code rather than style.
-                        \\* Resource allocation may fail; resource deallocation must succeed.
-                        \\* Memory is a resource.
-                        \\* Together we serve the users.
+                        \\A. Communicate intent precisely.
+                        \\B. Edge cases matter.
+                        \\C. Favor reading code over writing code.
+                        \\D. Only one obvious way to do things.
+                        \\E. Runtime crashes are better than bugs.
+                        \\F. Compile errors are better than runtime crashes.
+                        \\G. Incremental improvements.
+                        \\H. Avoid local maximums.
+                        \\I. Reduce the amount one must remember.
+                        \\J. Focus on code rather than style.
+                        \\K. Resource allocation may fail; resource deallocation must succeed.
+                        \\L. Memory is a resource.
+                        \\M. Together we serve the users.
                         \\```
                     },
                 });
